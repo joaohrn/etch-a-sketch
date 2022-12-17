5c233c2a76c648b96e9544c4d61d3c169e271954
joaohrn <joaohrn0@gmail.com>
2022-12-16 21:59:06 -0300

    Add create grid button

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
-const container = document.querySelector(".container");
-for (let i = 1; i <= 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("square");
-  container.appendChild(div);
+function createGrid(size) {
+  if (size > 100) {
+    alert("too many squares");
+    return;
+  }
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  for (let i = 1; i <= Math.pow(size, 2); i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
+  }
 }
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((square) => {
-  square.addEventListener("mouseenter", (e) => {
-    e.target.style.backgroundColor = "red";
+const creator = document.querySelector(".btn");
+creator.addEventListener("click", () => {
+  const choice = prompt("how many squares do you want");
+  createGrid(parseInt(choice));
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.style.width = `calc(960px/${choice})`;
+  });
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", (e) => {
+      e.target.style.backgroundColor = "red";
+    });
   });
 });
